c4096146295838e409c0ebfbcd786d124e5e94ae
FrancOlano <olanofranco1@gmail.com>
2022-05-04 13:20:34 +0200

    franco

--- Marcos.js
+++ Marcos.js
@@ -3,8 +3,4 @@
 
 // Cambios en la branch de Marcos.... probando 1,2,3
 
-<<<<<<< HEAD
-function suma 
-=======
 function suma ()
->>>>>>> 5ffc8bdbe5668f8daec024d171d7079aed926078
--- franco.js
+++ franco.js
@@ -1,10 +1,5 @@
 // hola buenas
 
-<<<<<<< HEAD
 let hola = true
 
 let nombre = 'franco'
-=======
-
-// Victoria reportandose
->>>>>>> f2c860ad579c852da43bb320761d297c0262fbd3
